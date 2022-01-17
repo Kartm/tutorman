e15f6a2a8f90b8541015d42f3c0bb9b623f92c17
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-01-17 13:18:12 +0100

    Add basic database operation

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,3 +7,27 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "tutorings.html"
+
+    def get_context_data(self, **kwargs):
+        with connection.cursor() as cursor:
+            query = """
+                SELECT *
+                FROM "Timeslot"
+                         LEFT JOIN timeslot_tutoringparticipant ttp ON timeslot.id_timeslot = ttp.id_timeslot
+                         LEFT JOIN "Medium" ON timeslot.id_medium = medium.id_medium
+                         LEFT JOIN "Tutoring" ON timeslot.id_tutoring = tutoring.id_tutoring
+                WHERE (id_timeslot_tutoring_participant IS NULL OR allow_multiple_participants IS 1)
+                  AND "Medium".is_remote = 1
+                  AND "Timeslot".takes_place_at BETWEEN DATETIME('2021-11-01 15:22:18') AND DATETIME('2021-11-13 15:22:18')
+                  AND "Tutoring".id_subject = 1
+                ORDER BY "Timeslot".takes_place_at;
+            """
+            cursor.execute(query)
+
+            columns = [col[0] for col in cursor.description]
+            rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+            print(rows)
+
+            context = super().get_context_data(**kwargs)
+            context['tutorings'] = rows
+            return context
